4fec46866f3ff281b0f67bf43c33ed169e3c92ae
Crisol Cova <crisolvalentian@gmail.com>
2024-01-18 10:16:45 -0400

    fix

--- service/signaturit.ts
+++ service/signaturit.ts
@@ -8,34 +8,34 @@ import {
   participacion,
   users_user,
 } from "@prisma/client";
+import fs from "fs";
+import mustache from "mustache";
+import puppeteer from "puppeteer";
+
 // replace it with your API key
 const API_KEY = process.env.SIGNATURITKEY;
 const client = new SignaturitClient(API_KEY);
 export const createSignature = async () => {
   try {
-    const document = await client.createSignature(
-      "Nota Convertible - Datos & Formulario.pdf",
-      {
-        name: `user.first_name`,
-        email: `crisolvalentina@gmail.com`,
-        role: "Signer",
-      },
-      {
-        templates: "#crear",
-        data: {
-          emp_nif: "827392",
-          emp_country: "ES",
-          emp_city: "Caracas",
-          emp_name: "Blockimpulse",
-          doc_date: new Date().toLocaleDateString(),
-        },
-        signature_widget: {
-          page: 1,
-          x: 100,
-          y: 200,
-        },
-      }
+    let plantilla = fs.readFileSync(
+      "emails/emails-user/Gestion User - Login con codigo de acceso.html",
+      "utf-8"
     );
+    const data = { "Document.date": new Date().toLocaleString() };
+    plantilla = mustache.render(plantilla, data);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(plantilla);
+
+    // Ajusta las opciones según tus necesidades
+    await page.pdf({ path: "crearCP.pdf", format: "A4" });
+
+    await browser.close();
+    const document = await client.createSignature("crearCP.pdf", {
+      name: `user.first_name`,
+      email: `crisolvalentina@gmail.com`,
+      role: "Signer",
+    });
     console.log(document);
     return document;
   } catch (e) {
