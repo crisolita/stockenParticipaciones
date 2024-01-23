3afbe6dae9d730e03bd67efacbd2174020c434db
Crisol Cova <crisolvalentian@gmail.com>
2024-01-23 13:55:48 -0400

    add routes

--- routes/cuentasParticipes.ts
+++ routes/cuentasParticipes.ts
@@ -3,9 +3,13 @@ import { authenticateToken } from "../middleware/auth";
 import { isAdmin } from "../middleware/isAdmin";
 import Joivalidator from "express-joi-validation";
 import {
+  aceptarCompraParticipacion,
   aceptarComprasCuentaParticipe,
   comprarParticipacion,
+  comprarParticipacionPorOrden,
   crearCuentaParticipe,
+  crearVentaDeParticipacion,
+  rechazarCompraParticipacion,
   rechazarComprasCuentaParticipe,
   signCompraDoc,
   verCuentasParticipes,
@@ -30,5 +34,12 @@ router.post("/verOrdenesBySell", verOrdenesBySell);
 router.post("/verOrdenesByBuyer", verOrdenesByBuyer);
 
 router.post("/verCuentasParticipes", verCuentasParticipes);
+
+//// MARKETPLACE
+
+router.post("/venderParticipacion", crearVentaDeParticipacion);
+router.post("/comprarParticipacion", comprarParticipacionPorOrden);
+router.post("/rechazarOrdenParticipacion", rechazarCompraParticipacion);
+router.post("/aceptarOrdenParticipacion", aceptarCompraParticipacion);
 
 export default router;
